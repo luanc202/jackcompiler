e3a89ac4b617c88e9520be3fa046409eff881241
Luan A. <barkon32@tuta.io>
2024-12-16 00:19:52 -0300

    feat: implement ParseError class

---
+++ src/main/java/br/ufma/ecp/ParseError.java
@@ -0,0 +1,11 @@
+package br.ufma.ecp;
+
+public class ParseError extends RuntimeException{
+
+    private String message;
+
+    public ParseError(String message) {
+        this.message = message;
+    }
+
+}
