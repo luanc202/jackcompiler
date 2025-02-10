3131bc6e9c496f2c505c93708454de5d77ea824c
Luan Alves <barkon32@tuta.io>
2025-02-10 00:39:41 -0300

    feat: try new methods

--- src/main/java/br/ufma/ecp/targetcode/CodeWriter.java
+++ src/main/java/br/ufma/ecp/targetcode/CodeWriter.java
@@ -7,12 +7,22 @@ import java.io.IOException;
 public class CodeWriter {
     private final BufferedWriter writer;
     private int labelCounter = 0;
+    private String fileName;
 
     public CodeWriter(String outputFile) throws IOException {
-        writer = new BufferedWriter(new FileWriter(outputFile, true)); // Modo append
+        writer = new BufferedWriter(new FileWriter(outputFile, false)); // Mode overwrite
     }
 
-    public String writePush(String segment, int index) throws IOException {
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void writeInit() throws IOException {
+        writer.write("@256\nD=A\n@SP\nM=D\n"); // Set SP to 256
+        writeCall("Sys.init", 0); // Call Sys.init
+    }
+
+    public void writePush(String segment, int index) throws IOException {
         String assemblyCode = switch (segment) {
             case "constant" -> "@" + index + "\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1";
             case "local" -> "@LCL\nD=M\n@" + index + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
@@ -21,24 +31,27 @@ public class CodeWriter {
             case "that" -> "@THAT\nD=M\n@" + index + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
             case "temp" -> "@" + (5 + index) + "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
             case "pointer" -> "@" + (3 + index) + "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
-            case "static" -> "@Foo." + index + "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
+            case "static" -> "@" + fileName + "." + index + "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
             default -> "";
         };
-        return assemblyCode + "\n";
+        writer.write(assemblyCode + "\n");
     }
 
-    public String writePop(String segment, int index) throws IOException {
+    public void writePop(String segment, int index) throws IOException {
         String assemblyCode = switch (segment) {
             case "local" -> "@LCL\nD=M\n@" + index + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D";
             case "argument" -> "@ARG\nD=M\n@" + index + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D";
-            case "this" -> "@THIS\nD=M\n@" + index + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D";
-            case "that" -> "@THAT\nD=M\n@" + index + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D";
+            case "this" -> "@THIS\nD=M\n@" + index + "\nA=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D";
+            case "that" -> "@THAT\nD=M\n@" + index + "\nA=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D";
+            case "temp" -> "@SP\nAM=M-1\nD=M\n@" + (5 + index) + "\nM=D";
+            case "pointer" -> "@SP\nAM=M-1\nD=M\n@" + (3 + index) + "\nM=D";
+            case "static" -> "@SP\nAM=M-1\nD=M\n@" + fileName + "." + index + "\nM=D";
             default -> "";
         };
-        return assemblyCode + "\n";
+        writer.write(assemblyCode + "\n");
     }
 
-    public String writeArithmetic(String command) throws IOException {
+    public void writeArithmetic(String command) throws IOException {
         String assemblyCode = "";
         switch (command) {
             case "add":
@@ -51,7 +64,8 @@ public class CodeWriter {
                 assemblyCode = "@SP\nA=M-1\nM=-M";
                 break;
             case "eq":
-                assemblyCode = "@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@TRUE\nD;JEQ\n@SP\nA=M-1\nM=0\n@END\n0;JMP\n(TRUE)\n@SP\nA=M-1\nM=-1\n(END)";
+                assemblyCode = "@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@EQ" + labelCounter + "\nD;JEQ\n@SP\nA=M-1\nM=0\n@END" + labelCounter + "\n0;JMP\n(EQ" + labelCounter + ")\n@SP\nA=M-1\nM=-1\n(END" + labelCounter + ")";
+                labelCounter++;
                 break;
             case "gt":
                 assemblyCode = "@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@GT" + labelCounter + "\nD;JGT\n@SP\nA=M\nM=0\n@END" + labelCounter + "\n0;JMP\n(GT" + labelCounter + ")\n@SP\nA=M\nM=-1\n(END" + labelCounter + ")";
@@ -60,48 +74,67 @@ public class CodeWriter {
             case "lt":
                 assemblyCode = "@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@LT" + labelCounter + "\nD;JLT\n@SP\nA=M\nM=0\n@END" + labelCounter + "\n0;JMP\n(LT" + labelCounter + ")\n@SP\nA=M\nM=-1\n(END" + labelCounter + ")";
                 labelCounter++;
+                break;
+            case "and":
+                assemblyCode = "@SP\nAM=M-1\nD=M\nA=A-1\nM=D&M";
+                break;
+            case "or":
+                assemblyCode = "@SP\nAM=M-1\nD=M\nA=A-1\nM=D|M";
+                break;
+            case "not":
+                assemblyCode = "@SP\nA=M-1\nM=!M";
+                break;
         }
-        return assemblyCode + "\n";
+        writer.write(assemblyCode + "\n");
     }
 
-    public String writeLabel(String label) throws IOException {
-        return ("(" + label + ")\n");
+    public void writeLabel(String label) throws IOException {
+        writer.write("(" + fileName + "$" + label.toUpperCase() + ")\n");
     }
 
-    public String writeGoto(String label) throws IOException {
-        return ("@SP\nA=M-1\nD=M\n@" + label + "\nD;JNE\n@SP\nM=M-1\n");
+    public void writeGoto(String label) throws IOException {
+        writer.write("@" + fileName + "$" + label.toUpperCase() + "\n0;JMP\n");
     }
 
-    public String writeIf(String label) throws IOException {
-        return ("@SP\nAM=M-1\nD=M\n@" + label + "\nD;JNE\n");
+    public void writeIf(String label) throws IOException {
+        writer.write("@SP\nAM=M-1\nD=M\n@" + fileName + "$" + label.toUpperCase() + "\nD;JNE\n");
     }
 
+    public void writeCall(String functionName, int nArgs) throws IOException {
+        String returnLabel = "RETURN$" + functionName + "$" + labelCounter;
+        labelCounter++;
 
-    public String writeCall(String functionName, int nArgs) throws IOException {
-        StringBuilder assemblyCode = new StringBuilder();
-        String returnLabel = "RETURN_" + functionName;
-        assemblyCode.append("@").append(returnLabel).append("\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
-        for (String segment : new String[]{"LCL", "ARG", "THIS", "THAT"}) {
-            assemblyCode.append("@").append(segment).append("\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
-        }
-        assemblyCode.append("@SP\nD=M\n@").append(nArgs + 5).append("\nD=D-A\n@ARG\nM=D\n");
-        assemblyCode.append("@SP\nD=M\n@LCL\nM=D\n");
+        writer.write("@" + returnLabel + "\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"); // Push return address
+        writer.write("@LCL\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"); // Push LCL
+        writer.write("@ARG\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"); // Push ARG
+        writer.write("@THIS\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"); // Push THIS
+        writer.write("@THAT\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"); // Push THAT
 
-        assemblyCode.append("@").append(functionName).append("\n0;JMP\n");
-        assemblyCode.append("(").append(returnLabel).append(")\n");
-
-        return assemblyCode.toString();
+        writer.write("@" + nArgs + "\nD=A\n@SP\nD=M-D\n@5\nD=D-A\n@ARG\nM=D\n"); // ARG = SP - nArgs - 5
+        writer.write("@SP\nD=M\n@LCL\nM=D\n"); // LCL = SP
+        writer.write("@" + functionName + "\n0;JMP\n"); // Jump to function
+        writer.write("(" + returnLabel + ")\n"); // Return address label
     }
 
-
-    public String writeFunction(String functionName, int nLocals) throws IOException {
-        StringBuilder assemblyCode = new StringBuilder();
-        assemblyCode.append("(").append(functionName).append(")\n");
+    public void writeFunction(String functionName, int nLocals) throws IOException {
+        writer.write("(" + functionName + ")\n");
         for (int i = 0; i < nLocals; i++) {
-            assemblyCode.append("@SP\nA=M\nM=0\n@SP\nM=M+1\n");
+            writer.write("@0\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
         }
-    
-        return assemblyCode.toString();
+    }
+
+    public void writeReturn() throws IOException {
+        writer.write("@LCL\nD=M\n@R13\nM=D\n"); // R13 = FRAME (FRAME is LCL)
+        writer.write("@5\nA=D-A\nD=M\n@R14\nM=D\n"); // R14 = RET (RET is FRAME - 5)
+
+        writer.write("@SP\nAM=M-1\nD=M\n@ARG\nA=M\nM=D\n"); // *ARG = pop()
+        writer.write("@ARG\nD=M+1\n@SP\nM=D\n"); // SP = ARG + 1
+
+        writer.write("@R13\nA=M-1\nD=M\n@THAT\nM=D\n"); // THAT = *(FRAME - 1)
+        writer.write("@2\nD=A\n@R13\nA=M-D\nD=M\n@THIS\nM=D\n"); // THIS = *(FRAME - 2)
+        writer.write("@3\nD=A\n@R13\nA=M-D\nD=M\n@ARG\nM=D\n"); // ARG = *(FRAME - 3)
+        writer.write("@4\nD=A\n@R13\nA=M-D\nD=M\n@LCL\nM=D\n"); // LCL = *(FRAME - 4)
+        writer.write("@R14\nA=M\n0;JMP\n"); // Jump to RET
     }
 
     public void close() throws IOException {
--- src/main/java/br/ufma/ecp/targetcode/VMApp.java
+++ src/main/java/br/ufma/ecp/targetcode/VMApp.java
@@ -8,79 +8,30 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Objects;
 
+import java.io.File;
+import java.io.IOException;
+
 public class VMApp {
-
-    public static void saveToFile(String fileName, String output) {
-        try (FileOutputStream outputStream = new FileOutputStream(fileName)) {
-            byte[] strToBytes = output.getBytes();
-            outputStream.write(strToBytes);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static String fromFile(File file) {
-        byte[] bytes;
-        try {
-            bytes = Files.readAllBytes(file.toPath());
-            return new String(bytes, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
-
     public static void main(String[] args) {
-        String outputFile = "program.asm";
-        StringBuilder fullAssemblyCode = new StringBuilder();
-
         if (args.length != 1) {
-            System.err.println("Please provide a single file path argument.");
-            System.exit(1);
+            System.err.println("Usage: java VMApp <directory>");
+            return;
         }
 
-        File file = new File(args[0]);
-        if (!file.exists()) {
-            System.err.println("The file doesn't exist.");
-            System.exit(1);
+        File directory = new File(args[0]);
+        if (!directory.isDirectory()) {
+            System.err.println("The provided path is not a directory.");
+            return;
         }
 
-
-        // Compilando cada arquivo .vm da pasta
-        if (file.isDirectory()) {
-            for (File f : Objects.requireNonNull(file.listFiles())) {
-                if (f.isFile() && f.getName().endsWith(".vm")) {
-                    System.out.println("compiling " + f.getAbsolutePath());
-                    String programavm = fromFile(f);
-                    try {
-                        VMTranslator translator = new VMTranslator(outputFile);
-                        String translatedCode = translator.translate(programavm);
-                        fullAssemblyCode.append(translatedCode);
-                    } catch (IOException e) {
-                        System.err.println("Erro ao traduzir o programa VM: " + e.getMessage());
-                        e.printStackTrace();
-                    }
-                }
-            }
-        } else if (file.isFile()) {
-            if (!file.getName().endsWith(".vm")) {
-                System.err.println("Please provide a file name ending with .vm");
-                System.exit(1);
-            }
-            System.out.println("compiling " + file.getAbsolutePath());
-            String programavm = fromFile(file);
-            try {
-                VMTranslator translator = new VMTranslator(outputFile);
-                String translatedCode = translator.translate(programavm);
-                fullAssemblyCode.append(translatedCode);
-            } catch (IOException e) {
-                System.err.println("Erro ao traduzir o programa VM: " + e.getMessage());
-                e.printStackTrace();
-            }
+        String outputFile = "program.asm";
+        try {
+            VMTranslator translator = new VMTranslator(outputFile);
+            translator.translateDirectory(directory);
+            System.out.println("Tradução concluída com sucesso! Arquivo gerado: " + outputFile);
+        } catch (IOException e) {
+            System.err.println("Erro ao traduzir os arquivos VM: " + e.getMessage());
+            e.printStackTrace();
         }
-
-        // Salva todo o código gerado no arquivo de saída
-        saveToFile(outputFile, fullAssemblyCode.toString());
-        System.out.println("Tradução concluída com sucesso! Arquivo gerado: " + outputFile);
     }
 }
