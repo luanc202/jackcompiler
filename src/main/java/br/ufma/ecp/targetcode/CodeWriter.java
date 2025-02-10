659fd0553fb6b82f397f4fbe08b3c4b83a736b2b
Luan Alves <barkon32@tuta.io>
2025-02-09 23:15:11 -0300

    feat: refactor

--- src/main/java/br/ufma/ecp/targetcode/CodeWriter.java
+++ src/main/java/br/ufma/ecp/targetcode/CodeWriter.java
@@ -5,78 +5,47 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class CodeWriter {
-    private BufferedWriter writer;
+    private final BufferedWriter writer;
 
     public CodeWriter(String outputFile) throws IOException {
         writer = new BufferedWriter(new FileWriter(outputFile, true)); // Modo append
     }
 
     public String writePush(String segment, int index) throws IOException {
-        String assemblyCode = "";
-        switch (segment) {
-            case "constant":
-                assemblyCode = "@" + index + "\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1";
-                break;
-            case "local":
-                assemblyCode = "@LCL\nD=M\n@" + index + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
-                break;
-            case "argument":
-                assemblyCode = "@ARG\nD=M\n@" + index + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
-                break;
-            case "this":
-                assemblyCode = "@THIS\nD=M\n@" + index + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
-                break;
-            case "that":
-                assemblyCode = "@THAT\nD=M\n@" + index + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
-                break;
-            case "temp":
-                assemblyCode = "@" + (5 + index) + "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
-                break;
-            case "pointer":
-                assemblyCode = "@" + (3 + index) + "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
-                break;
-            case "static":
-                assemblyCode = "@Foo." + index + "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
-                break;
-        }
+        String assemblyCode = switch (segment) {
+            case "constant" -> "@" + index + "\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1";
+            case "local" -> "@LCL\nD=M\n@" + index + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
+            case "argument" -> "@ARG\nD=M\n@" + index + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
+            case "this" -> "@THIS\nD=M\n@" + index + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
+            case "that" -> "@THAT\nD=M\n@" + index + "\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
+            case "temp" -> "@" + (5 + index) + "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
+            case "pointer" -> "@" + (3 + index) + "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
+            case "static" -> "@Foo." + index + "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
+            default -> "";
+        };
         return assemblyCode + "\n";
     }
 
     public String writePop(String segment, int index) throws IOException {
-        String assemblyCode = "";
-        switch (segment) {
-            case "local":
-                assemblyCode = "@LCL\nD=M\n@" + index + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D";
-                break;
-            case "argument":
-                assemblyCode = "@ARG\nD=M\n@" + index + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D";
-                break;
-            case "this":
-                assemblyCode = "@THIS\nD=M\n@" + index + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D";
-                break;
-            case "that":
-                assemblyCode = "@THAT\nD=M\n@" + index + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D";
-                break;
-        }
+        String assemblyCode = switch (segment) {
+            case "local" -> "@LCL\nD=M\n@" + index + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D";
+            case "argument" -> "@ARG\nD=M\n@" + index + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D";
+            case "this" -> "@THIS\nD=M\n@" + index + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D";
+            case "that" -> "@THAT\nD=M\n@" + index + "\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D";
+            default -> "";
+        };
         return assemblyCode + "\n";
     }
 
     public String writeArithmetic(String command) throws IOException {
-        String assemblyCode = "";
-        switch (command) {
-            case "add":
-                assemblyCode = "@SP\nAM=M-1\nD=M\nA=A-1\nM=M+D";
-                break;
-            case "sub":
-                assemblyCode = "@SP\nAM=M-1\nD=M\nA=A-1\nM=M-D";
-                break;
-            case "neg":
-                assemblyCode = "@SP\nA=M-1\nM=-M";
-                break;
-            case "eq":
-                assemblyCode = "@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@TRUE\nD;JEQ\n@SP\nA=M-1\nM=0\n@END\n0;JMP\n(TRUE)\n@SP\nA=M-1\nM=-1\n(END)";
-                break;
-        }
+        String assemblyCode = switch (command) {
+            case "add" -> "@SP\nAM=M-1\nD=M\nA=A-1\nM=M+D";
+            case "sub" -> "@SP\nAM=M-1\nD=M\nA=A-1\nM=M-D";
+            case "neg" -> "@SP\nA=M-1\nM=-M";
+            case "eq" ->
+                    "@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n@TRUE\nD;JEQ\n@SP\nA=M-1\nM=0\n@END\n0;JMP\n(TRUE)\n@SP\nA=M-1\nM=-1\n(END)";
+            default -> "";
+        };
         return assemblyCode + "\n";
     }
 
@@ -96,15 +65,15 @@ public class CodeWriter {
     public String writeCall(String functionName, int nArgs) throws IOException {
         StringBuilder assemblyCode = new StringBuilder();
         String returnLabel = "RETURN_" + functionName;
-        assemblyCode.append("@" + returnLabel + "\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
+        assemblyCode.append("@").append(returnLabel).append("\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
         for (String segment : new String[]{"LCL", "ARG", "THIS", "THAT"}) {
-            assemblyCode.append("@" + segment + "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
+            assemblyCode.append("@").append(segment).append("\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
         }
-        assemblyCode.append("@SP\nD=M\n@" + (nArgs + 5) + "\nD=D-A\n@ARG\nM=D\n");
+        assemblyCode.append("@SP\nD=M\n@").append(nArgs + 5).append("\nD=D-A\n@ARG\nM=D\n");
         assemblyCode.append("@SP\nD=M\n@LCL\nM=D\n");
 
-        assemblyCode.append("@" + functionName + "\n0;JMP\n");
-        assemblyCode.append("(" + returnLabel + ")\n");
+        assemblyCode.append("@").append(functionName).append("\n0;JMP\n");
+        assemblyCode.append("(").append(returnLabel).append(")\n");
 
         return assemblyCode.toString();
     }
